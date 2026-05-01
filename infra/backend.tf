9d25758298e2ebcdb866b4fc825aa55d63203d22
Sakuya Aradono <236024176+sakuyaxx21-sys@ users.noreply.github.com>
2026-05-01 21:46:52 +0900

    chore: configure remote Terraform state backend

---
+++ infra/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket       = "dev-portfolio-tfstate-139295583002"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-northeast-1"
+    profile      = "terraform-dev"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
